3d759f49668484961579bb8a04a47ed71f5da057
stephan <stephan102002@gmail.com>
2024-01-30 14:29:58 -0500

    The .env file was configutred

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 
-	"github.com/tesis/internal/users/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/tesis/internal/common/conf"
+	"github.com/tesis/internal/users/http"
 )
 
+type App struct {
+	env *conf.Env
+}
+
 func main() {
+
+	app := App{}
+
+	app.env = conf.NewEnv()
 	fmt.Println("hola stephan")
+
+	fmt.Println(app.env.MongoUsername)
+
 	r := gin.Default()
 	http.Routes(r)
 }
