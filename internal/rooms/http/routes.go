c9c7cc30430c7329d8d23aae220c6057a34399c6
stephan <stephan102002@gmail.com>
2024-04-30 16:22:17 -0500

    rooms-ms ready

--- internal/rooms/http/routes.go
+++ internal/rooms/http/routes.go
@@ -5,14 +5,14 @@ import (
 )
 
 func Routes(r *gin.Engine, appRoomDependencies *RoomDependencies) {
-	routesRooms := r.Group("/v1/rooms/")
-	routesRooms.GET("/", appRoomDependencies.rh.GetRooms)
-	routesRooms.POST("/", appRoomDependencies.rh.CreateRoom)
-	routesRooms.GET("/:id", appRoomDependencies.rh.GetRoomByID)
-	routesRooms.PUT("/:id", appRoomDependencies.rh.UpdateRoom)
-	routesRooms.DELETE("/:id", appRoomDependencies.rh.DeleteRoom)
+	routesRooms := r.Group("/v1/rooms")
+	routesRooms.GET("", appRoomDependencies.rh.GetRooms)
+	routesRooms.POST("", appRoomDependencies.rh.CreateRoom)
+	routesRooms.GET(":id", appRoomDependencies.rh.GetRoomByID)
+	routesRooms.PUT(":id", appRoomDependencies.rh.UpdateRoom)
+	routesRooms.DELETE(":id", appRoomDependencies.rh.DeleteRoom)
 
 	// add user to room
-	routesRooms.PUT("/add-user/:id", appRoomDependencies.rh.AddUserToRoom)
-	routesRooms.PUT("/remove-user/:id", appRoomDependencies.rh.RemoveUserInRoom)
+	routesRooms.PUT("add-user/:id", appRoomDependencies.rh.AddUserToRoom)
+	routesRooms.PUT("remove-user/:id", appRoomDependencies.rh.RemoveUserInRoom)
 }
